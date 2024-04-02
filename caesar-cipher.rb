3066c9252c9ce359335843020672b09e08faaa7f
Alan Sobchacki <53805387+alansobchacki@users.noreply.github.com>
2024-04-02 18:24:40 -0300

    Update caesar-cipher.rb
    Allow users to enter their phrases and also input a shift value to encode their strings.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,29 +1,32 @@
-def caesar_cipher(text, shift_value)
+# Encodes a string using caesar cipher - needs a string and a shift value to work.
+class CaesarCipher
+  def cipher(text, shift_value)
     alphabet_lowercase = ('a'..'z').to_a
     alphabet_uppercase = ('A'..'Z').to_a
-    cipher_text = ""
-    for i in 0..text.length - 1
-        if alphabet_lowercase.include?(text[i])
-            letter = alphabet_lowercase.index(text[i]) + shift_value
-            if letter >= 26
-                letter = letter % 26
-                cipher_text = cipher_text << alphabet_lowercase[letter]
-            else
-                cipher_text = cipher_text << alphabet_lowercase[letter]
-            end
-        elsif alphabet_uppercase.include?(text[i])
-            letter = alphabet_uppercase.index(text[i]) + shift_value 
-            if letter >= 26
-                letter = letter % 26
-                cipher_text = cipher_text << alphabet_uppercase[letter]
-            else 
-                cipher_text = cipher_text << alphabet_uppercase[letter]
-            end
-        else
-            cipher_text = cipher_text << text[i]
-        end
+    cipher_text = ''
+
+    text.each_char do |char|
+      if alphabet_lowercase.include?(char)
+        cipher_text << alphabet_lowercase[(alphabet_lowercase.index(char) + shift_value) % 26]
+      elsif alphabet_uppercase.include?(char)
+        cipher_text << alphabet_uppercase[(alphabet_uppercase.index(char) + shift_value) % 26]
+      else
+        cipher_text << char
+      end
     end
-    cipher_text
+
+    puts cipher_text
+  end
+
+  def change_string
+    puts "Please write the phrase you'd like to encode using Caesar's Cipher:"
+    string = gets.chomp
+    puts "Now, please enter the shift value you'd like to use. Enter a numerical value."
+    value = gets.chomp.to_i
+
+    cipher(string, value)
+  end
 end
 
-p caesar_cipher('What a string, Man!', 20)
+caesar = CaesarCipher.new
+caesar.change_string
